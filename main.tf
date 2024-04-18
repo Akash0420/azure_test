b6a0d1374332ce39c808f1c560dc5b1a456f9dde
Akash0420 <menonakash0420@gmail.com>
2024-04-19 03:15:23 +0530

    c16

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name = "cloud-shell-storage-centralindia"
+    storage_account_name = "csg10032003730c9146"
+  }
+}
 # Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "github-actions-rg"
@@ -19,7 +25,7 @@ resource "azurerm_storage_account" "storage" {
   name                = "githubactionstorage"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  account_tier        = "Standard_LRS"
+  account_tier        = "Standard"
   account_replication_type = "LRS"
 
   # Access for Service Principal
